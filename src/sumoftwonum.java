050195b3b233406ad52c62a4b473dc13c0639d54
Sim Akhter <simakhter1109@gmail.com>
2026-09-06 22:19:05 +0530

    Added first Java program

---
+++ src/sumoftwonum.java
@@ -0,0 +1,9 @@
+public class sumoftwonum {
+public static void main(String [] args){
+    System.out.print("The sum of two numbers is: ");
+    int num1= 3;
+    int num2= 4;
+    int sum= num1+num2;
+    System.out.println(sum);
+    }
+}
