2329a2108f9c5c2b283d4b09e712ded01870c587
Sim Akhter <simakhter1109@gmail.com>
2026-09-07 22:14:07 +0530

    Added Java Program

---
+++ src/NumCheck.java
@@ -0,0 +1,10 @@
+public class NumCheck{
+    public static void main (String[] args){
+        int num = 5;
+        if(num > 0){
+            System.out.println("Positive number.");
+        }else{
+            System.out.println("Negative number.");
+        }
+    }
+}
