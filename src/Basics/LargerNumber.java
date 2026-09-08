0160e8a474b9107ad578306727fda570fda40d88
Sim Akhter <simakhter1109@gmail.com>
2026-09-08 22:09:06 +0530

    Added java program

---
+++ src/Basics/LargerNumber.java
@@ -0,0 +1,11 @@
+public class LargerNumber {
+    public static void main (String [] args) {
+        int a = 15;
+        int b = 29;
+        if(a>b) {
+            System.out.println("Larger is " + a);
+        }else{
+            System.out.println("Larger is " + b);
+        }
+    }
+}
