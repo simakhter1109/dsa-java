3ebbfbdfa123e4940240f0f5045508f36d479f05
Sim Akhter <simakhter1109@gmail.com>
2026-09-18 23:59:21 +0530

    Added java program

--- src/DSA/Array/ArrayBasics.java
+++ src/DSA/Array/ArrayBasics.java
@@ -59,6 +59,24 @@ public class ArrayBasics {
         System.out.println("Even numbers = " + even );
         System.out.println("Odd numbers = " + odd);
 
+        // PROBLEM 7: COUNT POSITIVE, NEGATIVE AND ZEROS
+        int positive = 0;
+        int negative = 0;
+        int zeros = 0;
+        for(int i = 0; i < n; i ++) {
+            if (arr[i] > 0) {
+                positive++;
+            } else if (arr[i] < 0) {
+                negative++;
+            } else {
+                zeros++;
+            }
+        }
+        System.out.println("Positive numbers = " + positive);
+        System.out.println("Negative numbers = " + negative);
+        System.out.println("Zeros = " + zeros);
+
+
          sc.close();
     }
 }
