c98df6f4c01152759a4b9821a12cc84b77be49f0
Sim Akhter <simakhter1109@gmail.com>
2026-09-19 13:07:00 +0530

    Added java program

---
+++ src/Basics/ConditionsAndLoops.java
@@ -0,0 +1,12 @@
+public class ConditionsAndLoops{
+    public static void main (String[] args){
+
+        int salary = 254000;
+        if(salary > 100000){
+            salary = salary + 20000;
+        } else{
+            salary = salary + 10000;
+        }
+        System.out.println(salary);
+    }
+}
