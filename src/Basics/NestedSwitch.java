888c4008bd00e88c88fe34fd1806031b62487637
Sim Akhter <simakhter1109@gmail.com>
2026-09-22 01:04:39 +0530

    Added java program

--- src/Basics/NestedSwitch.java
+++ src/Basics/NestedSwitch.java
@@ -5,31 +5,46 @@ public class NestedSwitch{
         int empID = sc.nextInt();
         String department = sc.next();
 
-        switch (empID) {
-            case 1:
-                System.out.println("Sim Akhter");
-                break;
-            case 2:
-                System.out.println("Aayushi Banerjee");
-                break;
-            case 3:
-                System.out.println("Sejuti Sarah");
-                switch (department) {
-                    case "IT":
-                        System.out.println("IT Department");
-                        break;
-                    case "Management":
-                        System.out.println("Management Department");
-                        break;
-                    case "Finance":
-                        System.out.println("Finance Department");
-                    default:
-                        System.out.println("No Department entered");
-                }
+//        switch (empID) {
+//            case 1:
+//                System.out.println("Sim Akhter");
+//                break;
+//            case 2:
+//                System.out.println("Aayushi Banerjee");
+//                break;
+//            case 3:
+//                System.out.println("Sejuti Sarah");
+//                switch (department) {
+//                    case "IT":
+//                        System.out.println("IT Department");
+//                        break;
+//                    case "Management":
+//                        System.out.println("Management Department");
+//                        break;
+//                    case "Finance":
+//                        System.out.println("Finance Department");
+//                    default:
+//                        System.out.println("No Department entered");
+//                }
+//
+//                break;
+//            default:
+//                System.out.println("Enter correct EmpID");
+//        }
 
-                break;
-            default:
-                System.out.println("Enter correct EmpID");
+        switch (empID)  {
+            case 1 -> System.out.println("Sim Akhter");
+            case 2 -> System.out.println("Aayushi Banerjee");
+            case 3 -> System.out.println("Sejuti Sarah");
+            case 4 -> {
+                System.out.println("Emp number 3");
+                switch (department) {
+                    case "IT" -> System.out.println("IT Department");
+                    case "Management" -> System.out.println("Management Department");
+                    case "Finance" -> System.out.println("Finance Department");
+                }
+            }
+            default -> System.out.println("Enter correct EmpID");
         }
     }
 }
