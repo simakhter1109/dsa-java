0e90350cea691164f0c7182e64c4fa00bec104e9
Sim Akhter <simakhter1109@gmail.com>
2026-09-21 23:46:48 +0530

    Added java program

---
+++ src/Basics/Switch.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Switch{
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+//        String fruit = in.next();
+//
+
+        String a = "sim";
+        String b = "sim";
+        System.out.println(a == b);
+    }
+}
