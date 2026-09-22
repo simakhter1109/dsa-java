d654a620b9e90804ae13016a8f5e316c78c359de
Sim Akhter <simakhter1109@gmail.com>
2026-09-23 01:43:07 +0530

    Added java program

--- src/Basics/Sum.java
+++ src/Basics/Sum.java
@@ -1,7 +1,19 @@
 import java.util.Scanner;
 public class Sum {
     public static void main(String[] args){
-        sum();
+        int ans = sum2();
+        System.out.println(ans);
+    }
+
+    // return the value
+    static int sum2() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter number 2: ");
+        int num2 = sc.nextInt();
+        int sum = num1 + num2;
+        return sum;
     }
 
     static void sum() {
