3561f170491a87fa35a6509f9168a29892c450ac
Sim Akhter <simakhter1109@gmail.com>
2026-09-23 02:08:09 +0530

    Added java program

---
+++ src/Basics/GreetFunc.java
@@ -0,0 +1,26 @@
+public class GreetFunc {
+    public static void main(String[] args) {
+
+        String message = greet();
+        System.out.println(message);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.next();
+        String personalised = myGreet(name);
+        System.out.println(personalised);
+
+
+    }
+
+    static String myGreet(String name){
+        String message = "Hello " + name;
+        return message;
+    }
+
+    static String greet() {
+        String greeting = "How are you?";
+        return greeting;
+    }
+
+}
